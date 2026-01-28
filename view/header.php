ec33735caf3291607edeb7cb71dc5361039b2daf
Jay King <jking3@my.gulfcoast.edu>
2026-01-27 23:41:42 -0600

    Add files via upload

--- view/header.php
+++ view/header.php
@@ -1,6 +1,13 @@
 <header>
-	<img class="imgLogo" alt="GCSC logo" src="../assets/gcsc_logo.svg">
-	<br>
-	<h1>ACE Tutoring Lab</h1>
-	<br>
+  <div class="column-left">
+    <a href="../login">
+	<img alt="GCSC logo" src="../assets/gcsc_logo.svg" class="img-logo">
+</a>
+  </div>
+  <div class="column-center">
+    <h1>ACE Tutoring Lab</h1>
+  </div>
+  <div class="column-right">
+  	<img alt="GCSC commodore" src="../assets/commodore.png" class="img-c">
+  </div>
 </header>
